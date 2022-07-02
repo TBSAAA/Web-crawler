b839645a8250507822dbac740873df4433e46484
jack_huang <huanghaoranhoo@gmail.com>
2022-07-02 17:01:34 +1000

    movie_rank

--- case/test.py
+++ case/test.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 import time
+
 currentDateAndTime = datetime.now()
+
 
 # currentDateAndTime = currentDateAndTime.strftime("%Y/%m/%d")
 # print("The current date and time is", currentDateAndTime)
@@ -8,4 +10,29 @@ currentDateAndTime = datetime.now()
 
 # print (datetime.now()+timedelta(days=-1))
 # number_of_pagination =int(327653)/20 + 1
-print("image".format(time.time()))
+# print("image".format(time.time()))
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+if __name__ == "__main__":
+    str1 = '1'
+
+    str2 = '64.5'
+
+    str3 = '都撒到'
+
+    str4 = '–'
+
+    print(is_number(str1))
+
+    print(is_number(str2))
+
+    print(is_number(str3))
+
+    print(is_number(str4))
+
+    print(float(str2))
--- data_analysis/movie_rank.py
+++ data_analysis/movie_rank.py
@@ -2,27 +2,59 @@ import csv
 from itertools import islice
 
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 def top_20_rank():
     top_list = []
     lowest = 0
     with open("China_move_rank.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
         for row in islice(reader, 1, None):
+            # If there is no box office data, skip the movie.
+            if not is_number(row[3]):
+                continue
             if len(top_list) < 20:
                 top_list.append(row)
+                lowest = min(top_list, key=lambda x: float(x[3]))
             else:
-                lowest = min(top_list, key=lambda x: int(x[3]))
-                if row[3] > lowest and row[3].isdigit():
+                if float(row[3]) > float(lowest[3]):
                     top_list.append(row)
                     top_list.remove(lowest)
-    # top_list.sort(key=lambda x: int(x[3]), reverse=True)
+                    lowest = min(top_list, key=lambda x: float(x[3]))
+                else:
+                    continue
+    top_list.sort(key=lambda x: float(x[3]), reverse=True)
+    for i in top_list:
+        print(i)
 
 def bottom_20_rank():
+    bottom_list = []
+    highest = 0
     with open("China_move_rank.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
-            print(row)
-            break
+        for row in islice(reader, 1, None):
+            # If there is no box office data, skip the movie.
+            if not is_number(row[3]):
+                continue
+            if len(bottom_list) < 20:
+                bottom_list.append(row)
+                highest = max(bottom_list, key=lambda x: float(x[3]))
+            else:
+                if float(row[3]) < float(highest[3]):
+                    bottom_list.append(row)
+                    bottom_list.remove(highest)
+                    highest = max(bottom_list, key=lambda x: float(x[3]))
+                else:
+                    continue
+    bottom_list.sort(key=lambda x: float(x[3]), reverse=False)
+    for i in bottom_list:
+        print(i)
 
 
 if __name__ == "__main__":
